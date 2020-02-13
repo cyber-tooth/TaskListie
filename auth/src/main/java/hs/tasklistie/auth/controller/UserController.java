9d37a2f66647c71164a12f7274f0679a91e00650
Kseniia Tereshenkova <ksenia24t@gmail.com>
2020-02-13 17:19:00 +0100

    old things

--- auth/src/main/java/hs/tasklistie/auth/controller/UserController.java
+++ auth/src/main/java/hs/tasklistie/auth/controller/UserController.java
@@ -5,8 +5,9 @@ import hs.tasklistie.auth.dataTransferObject.SignupDto;
 import hs.tasklistie.auth.service.UserService;
 import hs.tasklistie.auth.web.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.context.request.WebRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -16,8 +17,18 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ModelAttribute("user")
+    public SignupDto signupDto() {
+        return new SignupDto();
+    }
+
+    @GetMapping("/registrieren")
+    public String signUpForm(Model model) {
+        return "/vt_register";
+    }
+
     @PostMapping("/registrieren")
-    public ApiResponse signUp(@Valid @RequestBody SignupDto signupDto){ return userService.signUp(signupDto); }
+    public ApiResponse signUp(@ModelAttribute("user") @Valid @RequestBody SignupDto signupDto){ return userService.signUp(signupDto); }
 
     @PostMapping("/login")
     public ApiResponse login(@Valid @RequestBody LoginDto loginDto){
