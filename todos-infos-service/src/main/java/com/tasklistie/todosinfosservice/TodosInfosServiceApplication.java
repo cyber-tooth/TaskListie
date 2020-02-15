cb40461b8cf56494c36f7a39e4a098ae4703c92e
ANNAFIW <sokolov-anna@outlook.de>
2020-02-15 20:04:06 +0100

    15.02.20

--- todos-infos-service/src/main/java/com/tasklistie/todosinfosservice/TodosInfosServiceApplication.java
+++ todos-infos-service/src/main/java/com/tasklistie/todosinfosservice/TodosInfosServiceApplication.java
@@ -2,7 +2,13 @@ package com.tasklistie.todosinfosservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableRetry
 @SpringBootApplication
 public class TodosInfosServiceApplication {
 
--- todos-infos-service/src/main/java/com/tasklistie/todosinfosservice/resources/TodosInfo.java
+++ todos-infos-service/src/main/java/com/tasklistie/todosinfosservice/resources/TodosInfo.java
@@ -1,5 +1,6 @@
 package com.tasklistie.todosinfosservice.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.tasklistie.todosinfosservice.models.Info;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodosInfo {
 
     @RequestMapping("/{todoID}")
+    @HystrixCommand(fallbackMethod = "getTodosInfo")
     public Info getTodosInfo(@PathVariable("todoID") String todoID){
 
-        return new Info(todoID, "Test name", false);
-
+        return new Info(todoID, "Arbeit", false);
     }
 }
