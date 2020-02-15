72e927e290dd6f1234a3cdf4c6e6351c145b0833
ANNAFIW <sokolov-anna@outlook.de>
2020-02-15 22:51:42 +0100

    Resilience patterns

--- todos-infos-service/src/main/java/com/tasklistie/todosinfosservice/service/CategoryService.java
+++ todos-infos-service/src/main/java/com/tasklistie/todosinfosservice/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.tasklistie.todosinfosservice.service;
 import com.tasklistie.todosinfosservice.models.CategoryEntity;
 import com.tasklistie.todosinfosservice.models.FavoriteEntity;
 import com.tasklistie.todosinfosservice.repository.CategoryRepository;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ public class CategoryService {
     public List<CategoryEntity> getAllUserCategory(Integer userId){
         return categoryRepository.findAllByUserId(userId);
     }
+
     public void addTaskToCategorie(Integer userId, Integer taskId) {
         categoryRepository.save(new CategoryEntity(userId, taskId));
     }
