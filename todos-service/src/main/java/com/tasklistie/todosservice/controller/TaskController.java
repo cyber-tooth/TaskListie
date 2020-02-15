f95b4a6b1d63c655c4ee8e798d0fc6d9d7e5a6b2
Adriyana Grotseva <s0527080@htw-berlin.de>
2020-02-15 23:21:04 +0100

    Todos comm

--- todos-service/src/main/java/com/tasklistie/todosservice/controller/TaskController.java
+++ todos-service/src/main/java/com/tasklistie/todosservice/controller/TaskController.java
@@ -1,9 +1,12 @@
 package com.tasklistie.todosservice.controller;
 
+import com.tasklistie.todosservice.DTO.TaskDTO;
 import com.tasklistie.todosservice.models.TaskItem;
 import com.tasklistie.todosservice.repo.TaskRepo;
 import com.tasklistie.todosservice.exception.ResourceNotFoundException;
+import com.tasklistie.todosservice.resources.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,9 +20,14 @@ public class TaskController {
     @Autowired
     private TaskRepo taskRepo;
 
+    @Autowired
+    private TaskService service;
+
     @GetMapping // GET to return all Tasks as list, which is also using JpaRepo extension
-    public List<TaskItem> findAll(){
-        return taskRepo.findAll();
+    public List<TaskItem> findAll(Model model){
+        model.addAttribute("tasks", service.findAll());
+        model.addAttribute("newTask", new TaskDTO());
+        return "tasks";
     }
 
     @PostMapping // POST, make sure that the saved task is not null and fulfills the restriction we made - not to be empty
